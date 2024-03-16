b24c85ac97c1c1ab1dfb5b67e40e09269710a332
afifa <2023afifa@gmail.com>
2024-03-16 19:22:39 +0600

    Banner responsive done

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -4,7 +4,7 @@ const Banner = () => {
             <div className="relative">
                 <img src="https://i.ibb.co/HVB6bJx/Banner-Image1.jpg" alt="" />
                 <div className="absolute inset-0 bg-black opacity-30"></div>
-                <h1 className="absolute lg:top-32 lg:left-10 lg:text-6xl text-gray-300 font-extrabold lg:max-w-2xl">Step into Elegance: Unveil Your Style with Classy Closet – Where Fashion Meets Passion! Explore a World of Glamour and Discover the Art of Timeless Dressing.</h1>
+                <h1 className="absolute top-14 md:top-24 lg:top-32 left-3 md:left-6 lg:left-10 text-xl md:text-4xl lg:text-6xl text-gray-300 font-extrabold max-w-xs md:max-w-lg lg:max-w-2xl">Step into Elegance: Unveil Your Style with Classy Closet – Where Fashion Meets Passion! Explore a World of Glamour and Discover the Art of Timeless Dressing.</h1>
             </div>
         </div>
     );
