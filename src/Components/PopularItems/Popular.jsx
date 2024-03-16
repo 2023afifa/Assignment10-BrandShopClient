2709ccf5b4d508a73f8bb9d389791d01369f1865
afifa <2023afifa@gmail.com>
2024-03-16 20:46:34 +0600

    Most Loved page responsive done

--- src/Components/PopularItems/Popular.jsx
+++ src/Components/PopularItems/Popular.jsx
@@ -2,6 +2,7 @@ import { useLoaderData } from "react-router-dom";
 import Navbar from "../Navbar/Navbar";
 import PopularItem from "./PopularItem";
 import { useEffect, useState } from "react";
+import Footer from "../Footer/Footer";
 
 
 const Popular = () => {
@@ -25,13 +26,14 @@ const Popular = () => {
             <div className="mt-20">
                 <div style={{ backgroundImage: 'url(https://i.ibb.co/94GddTF/popular2.jpg)' }}>
                     <h2 className="font-medium text-5xl text-center py-10">Our Most <span className="italic font-extrabold">Popular</span> Products</h2>
-                    <div className="grid lg:grid-cols-3 gap-10 pb-20">
+                    <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-10 pb-20 mx-5">
                         {
                             popularItems.map(card => <PopularItem key={card.id} card={card}></PopularItem>)
                         }
                     </div>
                 </div>
             </div>
+            <Footer></Footer>
         </div>
     );
 };
