f89c14038e0c1545893e2f866be11480afca3f32
afifa <2023afifa@gmail.com>
2024-03-16 19:34:51 +0600

    Collections section responsive done

--- src/Components/Collections/BrandCard.jsx
+++ src/Components/Collections/BrandCard.jsx
@@ -6,7 +6,7 @@ const BrandCard = ({ card }) => {
     return (
         <div className="mx-2 my-3 relative group overflow-hidden">
             <Link to={`/brands/${brand}`}>
-                <div className="relative">
+                <div className="relative w-96 mx-auto">
                     <figure><img className="w-96 h-[520px] mx-auto" src={brandCover} alt="" /></figure>
                     <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-50 transition-opacity">
                         <h4 className="text-4xl text-white absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">VIEW</h4>
